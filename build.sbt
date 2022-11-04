f03abb31a01bf713b785f851a18bdb0de4f21130
Alexandre LEBRUN <alexandre.lebrun@iadvize.com>
2022-11-04 20:35:52 +0100

    Update dependencies

--- build.sbt
+++ build.sbt
@@ -1,5 +1,5 @@
 val scala212 = "2.12.15"
-val scala213 = "2.13.8"
+val scala213 = "2.13.10"
 
 inThisBuild(
   List(
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val scalaXml = "org.scala-lang.modules" %% "scala-xml" % "2.0.1"
-  lazy val catsCore = "org.typelevel" %% "cats-core" % "2.7.0"
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.11"
+  lazy val scalaXml = "org.scala-lang.modules" %% "scala-xml" % "2.1.0"
+  lazy val catsCore = "org.typelevel" %% "cats-core" % "2.8.0"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.14"
 }
